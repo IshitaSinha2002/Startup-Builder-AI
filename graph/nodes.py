612dc42dbbb77c83baa9ca55adcd9ce35e21ca9e
Ishita Sinha <ishitasinha16@gmail.com>
2026-08-13 12:40:10 +0530

    Adding milestones to node

--- graph/nodes.py
+++ graph/nodes.py
@@ -6,11 +6,13 @@ from langchain_groq import ChatGroq
 from graph.state import (
     StartupState,
     StartupAnalysis,
-    MVPStrategy
+    MVPStrategy,
+    MilestonePlan
 )
 from prompts.prompts import (
     STARTUP_ANALYSIS_PROMPT,
-    MVP_STRATEGY_PROMPT
+    MVP_STRATEGY_PROMPT,
+    MILESTONE_PLANNING_PROMPT
 )
 
 load_dotenv()
@@ -22,6 +24,7 @@ llm = ChatGroq(
 
 structured_llm = llm.with_structured_output(StartupAnalysis)
 mvp_llm = llm.with_structured_output(MVPStrategy)
+milestone_llm = llm.with_structured_output(MilestonePlan)
 
 def analyze_startup(state: StartupState):
     startup_idea = state["startup_idea"]
@@ -49,4 +52,21 @@ def plan_mvp(state: StartupState):
 
     return {
         "mvp_strategy": response
+    }
+
+def plan_milestones(state: StartupState):
+    startup_idea = state["startup_idea"]
+    startup_analysis = state["startup_analysis"]
+    mvp_strategy = state["mvp_strategy"]
+
+    prompt = MILESTONE_PLANNING_PROMPT.format(
+        startup_idea=startup_idea,
+        startup_analysis=startup_analysis.model_dump(),
+        mvp_strategy=mvp_strategy.model_dump()
+    )
+
+    response = milestone_llm.invoke(prompt)
+
+    return {
+        "milestones": response.milestones
     }
